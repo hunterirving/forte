65936ebaaca5bff07952cb35962dc5555d26e347
hunterirving <hunter@hunterirving.com>
2020-09-28 17:01:48 -0400

    Only update when UI changes. "ATLAS" onRestart()

--- app/src/main/java/hunterirving/forte/FullscreenActivity.java
+++ app/src/main/java/hunterirving/forte/FullscreenActivity.java
@@ -32,7 +32,6 @@ public class FullscreenActivity extends AppCompatActivity {
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
 
@@ -46,9 +45,21 @@ public class FullscreenActivity extends AppCompatActivity {
 
         final TextView UI_container = findViewById(R.id.UI_container);
         final TextView selected_item = findViewById(R.id.selected_item);
+        //Initialize UI
+        String UI_backgroundString = "";
+        for (int i = 0; i < (appPairs.length) - index; i++) {
+            UI_backgroundString += "\n";
+        }
+        for (int i = 0; i < appPairs.length; i++) {
+            UI_backgroundString += appPairs[i][0] + "\n";
+        }
+        for (int i = 0; i < index; i++) {
+            UI_backgroundString += "\n";
+        }
+        UI_container.setText(UI_backgroundString);
+        selected_item.setText(appPairs[index][0]);
+
         final FrameLayout Frame = findViewById(R.id.Frame);
-
-
 
         Frame.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -84,6 +95,24 @@ public class FullscreenActivity extends AppCompatActivity {
 
                     //determine if UI needs to be updated
                     if (index != prevIndex) {
+                        String UI_backgroundString = "";
+
+                        //rebuild TextView strings
+                        //beginning
+                        for (int i = 0; i < (appPairs.length) - index; i++) {
+                            UI_backgroundString += "\n";
+                        }
+                        //middle
+                        for (int i = 0; i < appPairs.length; i++) {
+                            UI_backgroundString += appPairs[i][0] + "\n";
+                        }
+                        //end
+                        for (int i = 0; i < index; i++) {
+                            UI_backgroundString += "\n";
+                        }
+                        //update TextViews
+                        UI_container.setText(UI_backgroundString);
+                        selected_item.setText(appPairs[index][0]);
 
                         Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -94,25 +123,7 @@ public class FullscreenActivity extends AppCompatActivity {
                     }
 
 
-                    String UI_backgroundString = "";
 
-                    //rebuild TextView strings
-                    //beginning
-                    for (int i = 0; i < (appPairs.length) - index; i++) {
-                        UI_backgroundString += "\n";
-                    }
-                    //middle
-                    for (int i = 0; i < appPairs.length; i++) {
-                        UI_backgroundString += appPairs[i][0] + "\n";
-                    }
-                    //end
-                    for (int i = 0; i < index; i++) {
-                        UI_backgroundString += "\n";
-                    }
-
-                    //update TextViews
-                    UI_container.setText(UI_backgroundString);
-                    selected_item.setText(appPairs[index][0]);
 
                     return true;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
@@ -124,9 +135,15 @@ public class FullscreenActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+
+
+
+
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onRestart(){
         super.onRestart();
@@ -135,98 +152,27 @@ public class FullscreenActivity extends AppCompatActivity {
 
         lastKnownY = 0;
         yDelta = 0;
-        setContentView(R.layout.activity_fullscreen);
 
-        this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
+        //Re-Initialize UI
         final TextView UI_container = findViewById(R.id.UI_container);
         final TextView selected_item = findViewById(R.id.selected_item);
-        final FrameLayout Frame = findViewById(R.id.Frame);
 
+        String UI_backgroundString = "";
+        for (int i = 0; i < (appPairs.length) - index; i++) {
+            UI_backgroundString += "\n";
+        }
+        for (int i = 0; i < appPairs.length; i++) {
+            UI_backgroundString += appPairs[i][0] + "\n";
+        }
+        for (int i = 0; i < index; i++) {
+            UI_backgroundString += "\n";
+        }
+        UI_container.setText(UI_backgroundString);
+        selected_item.setText(appPairs[index][0]);
 
-
-        Frame.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    lastKnownY = event.getY();
-
-                    return true;
-                } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                    float newY = event.getY(); //get most recent Y position
-                    yDelta = lastKnownY - newY; //determine delta from last known position
-                    lastKnownY = newY; //store most recent Y position
-
-
-                    //Add newest movement delta to position
-                    float unclampedPos = pos + yDelta;
-
-                    //sanity check (clamp pos within UI bounds)
-                    if (unclampedPos < 0) {
-                        pos = 0;
-                    } else if (unclampedPos >= maxPos) {
-                        pos = maxPos - 1;
-                    } else {
-                        pos = unclampedPos;
-                    }
-
-                    //invert direction?
-
-
-                    //determine index of item that needs to be selected
-                    int prevIndex = index;
-                    index = (int) (pos / chunkSize); //0, 1, 2, or 3
-
-                    //determine if UI needs to be updated
-                    if (index != prevIndex) {
-
-                        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            vibrator.vibrate(VibrationEffect.createOneShot(25, 55));
-                        } else {
-                            vibrator.vibrate(55);
-                        }
-                    }
-
-
-                    String UI_backgroundString = "";
-
-                    //rebuild TextView strings
-                    //beginning
-                    for (int i = 0; i < (appPairs.length) - index; i++) {
-                        UI_backgroundString += "\n";
-                    }
-                    //middle
-                    for (int i = 0; i < appPairs.length; i++) {
-                        UI_backgroundString += appPairs[i][0] + "\n";
-                    }
-                    //end
-                    for (int i = 0; i < index; i++) {
-                        UI_backgroundString += "\n";
-                    }
-
-                    //update TextViews
-                    UI_container.setText(UI_backgroundString);
-                    selected_item.setText(appPairs[index][0]);
-
-                    return true;
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Intent launchIntent = getPackageManager().getLaunchIntentForPackage(appPairs[index][1]);
-                    startActivity(launchIntent);
-                }
-
-                return false;
-
-            }
-        });
 
     }
+
 }
 
 //TODO:
